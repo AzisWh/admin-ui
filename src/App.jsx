3bb46eba32650f161e7c19456db17d7a37e69401
azisssss <aziswh7@gmail.com>
2024-04-01 22:48:55 +0700

    tambahan teks dan link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,19 +17,20 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Tugas 2 Vercel Hibatul Azis</p>
+      <p>A11.2021.13868</p>
+      <a href="https://punggurbesar-web.vercel.app/">
+        <p>website profile desa yang saya buat dengan template next dari vercel</p>
+      </a>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
